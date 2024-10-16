f30791cbbba7fcf0130e998bea07b409760c0b09
dima217 <dimapikulik217@mail.com>
2024-10-16 21:22:26 +0300

    Initial commit

---
+++ backend/FaerieFitBack-app/src/main/java/org/faeriefit/project/FaerieFitBackAppApplication.java
@@ -0,0 +1,13 @@
+package org.faeriefit.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaerieFitBackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaerieFitBackAppApplication.class, args);
+	}
+
+}
---
+++ backend/FaerieFitBack-app/src/test/java/org/faeriefit/project/FaerieFitBackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.faeriefit.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaerieFitBackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
